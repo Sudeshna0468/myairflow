04a80e2efe5415974115d8020a24d520c96b76be
sudeshna.dandwate <sudeshna.dandwate@PDC2-L-C6W7FL3>
2025-02-19 11:58:39 +0530

    changes

--- airflow-docker/dags/minha_primeira_dag.py
+++ airflow-docker/dags/minha_primeira_dag.py
@@ -1,12 +1,35 @@
-import datetime
-
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.utils.dates import days_ago
 
-with DAG(
-    dag_id="meu_nome_de_dag",
-    start_date=datetime.datetime(2021, 1, 1),
-    schedule="@daily",
-    catchup=False
-):
-    EmptyOperator(task_id="tarefa")
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+}
+
+dag = DAG(
+    'example_postgres',
+    default_args=default_args,
+    schedule_interval='@daily',
+)
+
+# Task to read from the database
+read_task = PostgresOperator(
+    task_id='read_from_db',
+    postgres_conn_id='your_postgres_connection',
+    sql='SELECT * FROM your_table;',
+    dag=dag,
+)
+
+# Task to write to the database
+write_task = PostgresOperator(
+    task_id='write_to_db',
+    postgres_conn_id='your_postgres_connection',
+    sql='''
+    INSERT INTO your_table (column1, column2)
+    VALUES ('value1', 'value2');
+    ''',
+    dag=dag,
+)
+
+read_task >> write_task
